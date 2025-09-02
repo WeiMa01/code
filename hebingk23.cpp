c5717eaff24a22dff80e9bdd517dde9363396fcd
WeiMa01 <54025805+WeiMa01@users.noreply.github.com>
2025-09-02 14:24:44 +0800

    Update hebingk23.cpp

--- hebingk23.cpp
+++ hebingk23.cpp
@@ -13,49 +13,6 @@ using namespace std;
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
 
-class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* root = nullptr,* tmp = nullptr;
-        int k = lists.size();
-        int m, minVal;
-        vector<ListNode*>::iterator position;
-        if(k == 0){
-            return nullptr;
-        }
-        while(lists.size() > 0){
-            m = lists.size();
-            minVal = 10e4+1;
-            for(vector<ListNode*>::iterator i = lists.begin(); i != lists.end() ; i++){
-                if((*i) == nullptr){
-                    lists.erase(i);
-                }
-                if( (*i) -> val < minVal){
-                    position = i;
-                    minVal = (*i)->val;
-                }
-                //遍历每个 list 的头结点，找最小的
-            }
-            // 
-            if(root == nullptr){
-                root = *position;
-            }else{
-                tmp ->next = *position;
-            }
-            tmp = *position;
-
-            *position = (*position) ->next;
-            //删除表头元素
-            // if((*position) -> next != nullptr){
-            //     *position = (*position) ->next;
-            // }else{
-            //     lists.erase(position);
-            // }
-            tmp->next = nullptr;  
-        }
-        return root;
-    }
-};
 
 
 int main()
