663511106dfaac9e3db5e7a028c3c5b6cec153bf
WeiMa01 <54025805+WeiMa01@users.noreply.github.com>
2025-09-16 17:51:36 +0800

    Update [2197]替换数组中的非互质数.cpp

--- [2197]替换数组中的非互质数.cpp
+++ [2197]替换数组中的非互质数.cpp
@@ -1,84 +1,136 @@
-struct Node{
-    int val;
-    Node * next;
-    Node(){}
-    Node(int v){
-        val = v;
-        next = nullptr;
-    }
-    Node(int v, Node* p){
-        val = v;
-        next = p;
-    }
-};
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+
 
 class Solution {
 public:
-    int GCD(int a, int b){
+    // int GCD(int a, int b){
+    //     //最大公约数
+    //     int min_num =  a < b ? a :b;
+    //     for (int i = min_num; i > 0; i--){
+    //         if(a % i == 0 && b%i == 0){
+    //             return i;
+    //         }
+    //     }
+    //     return 1;
+    // }
+    // int LCM(int a, int b){
+    //     //  最小公倍数
+    //     int max_num = a > b ? a: b;
+    //     int min_num = a< b ? a:b;
+    //     for (int i = max_num; i / max_num <= min_num; i++) {
+    //         if(i % a == 0 && i %b ==0){
+    //             return i;
+    //         }
+    //     }
+    //     return a*b;
+    // }
+
+int GCD(int a, int b){
         //最大公约数
         int min_num =  a < b ? a :b;
-        for (int i = min_num; i > 0; i--)
-        {
-            if(a % i == 0 && b%i == 0){
-                return i;
-            }
+        int max_num =  a >= b ? a:b;
+        int c = max_num % min_num;
+        while(c!= 0){
+            max_num = b;
+            min_num = c;
+            c = max_num % min_num;
         }
-        return 1;
+        return min_num;
     }
     int LCM(int a, int b){
         //  最小公倍数
-        int max_num = a > b ? a: b;
-        for (int i = max_num; i <= a * b; i++)
-        {
-            if(i % a == 0 && i %b ==0){
-                return i;
-            }
-        }
-        return a*b;
+
+        return a * (b / GCD(a,b));
     }
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        //有很多删除操作，尝试使用链表
         int n = nums.size();
         if (n == 1) return nums;
-        Node * head = nullptr, *p = nullptr;
         vector<int> result;
-        //构建链表
-        for (int i = 0; i < n; i++)
-        {
-            cout << nums[i] << endl;
-            Node *new_node = new Node(nums[i]);
-            if (i == 0) {
-                head = new_node;
-                p = head;
+        result.push_back(nums[0]);
+        for (int i = 1; i < n; i++){
+            cout << result.back() << "  " << nums[i] << "  " << GCD(result.back(), nums[i]) << endl;
+            if(GCD(result.back(), nums[i]) != 1){
+                int tmp = result.back();
+                result.pop_back();
+                result.push_back(LCM(tmp, nums[i]));
+                //处理result中新加入的数和老数之间不满足条件
+                int n_size = result.size();
+                for(int j = n_size -1; j>0;j--){
+                    if(GCD(result[j], result[j-1]) != 1){
+                        int a = result.back();
+                        result.pop_back();
+                        int b = result.back();
+                        result.pop_back();
+                        result.push_back(LCM(a,b));
+                    }else{
+                        break;
+                    }
+                }
+
             }else{
-                p->next = new_node;
-                p = p-> next;
+                cout << "result .push back " << nums[i] << endl;
+                result.push_back(nums[i]);
             }
-        }
-        p = head;
-        Node * next_p = p-> next;
-        int num1, num2, num3;
-        while(next_p != nullptr){
-            num1 = p->val;
-            num2 = next_p->val;
-            if( GCD(num1, num2) != 1){
-                //有最大公约数
-                num3 = LCM(num1, num2);
-                // 删除 next_p 这个节点
-                p->val = num3;
-                p->next = next_p->next;
-                delete next_p;
-                next_p = p->next;
-            }else{
-                p = p->next;
-                next_p = next_p-> next;
+            cout << " result 中的元素" << endl;
+            for(int k = 0 ; k < result.size(); k++){
+                cout << result[i] << " ";
             }
-        }
-        p = head;
-        while(p != nullptr){
-            result.push_back(p -> val);
-            p = p-> next;
+            cout << endl;
         }
         return result; 
     }
 };
+
+
+
+int main()
+{
+
+    vector<int> test;
+    for (int i = 0; i < 10; i++)
+    {
+        test.push_back(i) ;
+    }
+    test.pop_back();
+    for(int i= 0 ; i< test.size() ;i++){
+        cout << test[i] << " ";
+    }
+
+    
+
+    // Solution solution;
+    // vector<int> nums = {602,37963,37963,37963};
+
+    // // cout << solution.GCD(37963, 37963) << "   " <<solution.LCM(37963, 37963)  << endl;
+
+
+    // auto result = solution.replaceNonCoprimes(nums);
+    // // // // cout << "result: " << result << std::endl;
+    // for (int i = 0; i < result.size(); i++) {
+    //     cout << result[i] << "   ";
+    // }
+    // for (int i = 0; i < result.size(); i++) {
+    //     for (int j = 0; j < result[i].size(); j++)
+    //     {
+    //         cout << result[i][j] << "    ";
+    //     }
+    //     cout << endl;
+        
+    // }
+
+    return 0;
+}
+//         (1 + 2) * (3 + 4)           (1 + 2) * 3 + 4                   //站里面 
+//         1 2 + 3 4 + *                1  2 + 3 * 4 +                   1 2 + 3 *   
